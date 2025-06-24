24adcb1b4fe6a02df988d4b7f23d76f028dfca63
dongyoon7212 <dongyoon7212>
2025-06-24 20:58:16 +0900

    feat: security

--- src/main/java/com/koreait/SpringSecurityStudy/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/koreait/SpringSecurityStudy/security/filter/JwtAuthenticationFilter.java
@@ -1,15 +1,30 @@
 package com.koreait.SpringSecurityStudy.security.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("전처리");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        //해당 메소드가 아니면 그냥 다음 필터로 넘김
+        List<String> methods = List.of("POST", "PUT", "GET", "PATCH", "DELETE");
+        if (!methods.contains(request.getMethod())) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+
+
+
+
+
+
         filterChain.doFilter(servletRequest, servletResponse);
         System.out.println("후처리");
     }
--- src/main/java/com/koreait/SpringSecurityStudy/service/AuthService.java
+++ src/main/java/com/koreait/SpringSecurityStudy/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.koreait.SpringSecurityStudy.service;
 
+import com.koreait.SpringSecurityStudy.dto.ApiRespDto;
+import com.koreait.SpringSecurityStudy.dto.SignupReqDto;
 import com.koreait.SpringSecurityStudy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +12,13 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public ApiRespDto<?> addUser(SignupReqDto signupReqDto) {
+        int result = userRepository.addUser(signupReqDto.toEntity(bCryptPasswordEncoder));
+        return new ApiRespDto<>("success", "회원가입 성공", result);
+    }
 
 }
